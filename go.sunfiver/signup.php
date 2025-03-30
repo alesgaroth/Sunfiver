48e60e023e9cf53a5f5ad6602877f0dcc539a92b
Alan Linton <alesgaroth@gmail.com>
2025-03-30 22:42:43 +0000

    redirect

--- go.sunfiver/signup.php
+++ go.sunfiver/signup.php
@@ -3,7 +3,10 @@ if (isset($_POST['email'])) {
     $mail = $_POST['email'];
     $name = $_POST['name'];
     mail('alesgaroth@alesgaroth.com', 'New signup', "Name: $name\nEmail: $mail");
-    mail($mail, 'Welcome to Sunfiver', "Hello $name,\n\nThank you for signing up for Sunfiver! We are excited to have you on board.\n\nBest regards,\nThe Sunfiver Team");
+    mail($mail, 'Welcome to Sunfiver', "Hello $name,\n\nThank you for signing up for Sunfiver! We are excited to have you on board.\n\nBest regards,\nAlan Linton of The Sunfiver Team");
     redirect('https://sunfiver.com/thank-you');
+} else {
+    // Redirect to the signup page if accessed directly
+    redirect('https://sunfiver.com/');
 }
 ?>
