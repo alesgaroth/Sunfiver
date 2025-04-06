b715a5f12981204541eb4ef6dd8203ccc015f8f7
Alan Linton <alesgaroth@gmail.com>
2025-04-06 20:42:39 +0000

    I think I got the survey working

--- go.sunfiver/submit-survey.php
+++ go.sunfiver/submit-survey.php
@@ -2,7 +2,14 @@
 if (isset($_POST['email'])) {
     $mail = $_POST['email'];
     $name = $_POST['name'];
-    mail('alesgaroth@alesgaroth.com', 'sunfiver survey 1', $_POST);
+    $message = "";
+    foreach($_POST as $key => $value){
+        $message .= "{$key} : {$value}\n";
+    }
+    $headers = 'From: form@sunfiver.com' . "\r\n" .
+    'Reply-To: alesgaroth@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    mail('alesgaroth@alesgaroth.com', 'sunfiver survey 1', $message, $headers);
     header('Location: https://sunfiver.com/thank-you');
     ?>Thank you! We'll be in touch<?php
 } else {
