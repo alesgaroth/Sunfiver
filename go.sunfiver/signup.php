109c3982d3e55bad763e27a57fbbb21bfdc4a1a4
Alan Linton <alesgaroth@gmail.com>
2025-06-03 07:13:24 -0400

    canonical

--- go.sunfiver/signup.php
+++ go.sunfiver/signup.php
@@ -32,15 +32,15 @@ if (isset($_POST['email'])) {
     if (verifyCaptcha($recaptchaResponse)) {
       mail('alesgaroth@alesgaroth.com', 'New sunfiver signup', "Name: $name\nEmail: $mail\nRecaptcha: $recaptchaResponse");
       mail($mail, 'Welcome to Sunfiver', "Hello $name,\n\nThank you for signing up for Sunfiver! We are excited to have you on board.\n\nBest regards,\nAlan Linton of The Sunfiver Team");
-      header('Location: https://sunfiver.com/thank-you');
+      header('Location: https://www.sunfiver.com/thank-you');
     } else {
       // captcha failed.  go away
-      header('Location: https://sunfiver.com/');
+      header('Location: https://www.sunfiver.com/');
     }
     ?>Thank you! We'll be in touch<?php
 } else {
     // Redirect to the signup page if accessed directly
-    header('Location: https://sunfiver.com/');
+    header('Location: https://www.sunfiver.com/');
     ?>Hello?
     <?php
 }
